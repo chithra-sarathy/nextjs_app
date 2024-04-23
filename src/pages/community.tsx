9e8ed2738bfca461093e37722ea71250b4c175c3
Chithra <139841180+chithra-sarathy@users.noreply.github.com>
2024-04-23 03:52:05 +0000

    folder change-1

--- src/pages/community.tsx
+++ src/pages/community.tsx
@@ -1,3 +1,10 @@
+<<<<<<< Updated upstream:src/pages/community.tsx
 export default function mealsPage() {
   return <h1>Food Community and Policy</h1>;
 }
+=======
+export default function mealsPage() {
+  console.log("client side rendering");
+  return <h1>Food Community and Policy</h1>;
+}
+>>>>>>> Stashed changes:next-app/src/pages/community.tsx
